cee77dc7e9836bc90ebf74fcd022acfc959532de
Keks <keks@htmlacademy.ru>
2024-10-23 19:11:02 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/functions.js
@@ -0,0 +1,17 @@
+function lineSize(string, length){
+  return string.length <= length;
+}
+lineSize('проверяемая строка', 20);
+function isPalindrome (str){
+  const string = str.replaceAll(' ', '').toLowerCase();
+  for (let n = 0; n < string.length / 2; n++){
+    if (string.at(n) !== string.at(- n - 1)){
+      return false;
+    }
+  }
+  return true;
+}
+isPalindrome('топот');
+isPalindrome('ДовОд');
+isPalindrome('Кекс');
+isPalindrome('Лёша на полке клопа нашёл');
