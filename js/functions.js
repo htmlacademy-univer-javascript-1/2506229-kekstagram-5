20acfd39848aafdea79f772aa9d538d22de3561a
A-lina-Ser-kova <alinaserkova11@gmail.com>
2024-12-01 19:35:42 +0500

    Функции возвращаются

--- js/functions.js
+++ js/functions.js
@@ -1,17 +1,18 @@
-function lineSize(string, length){
-  return string.lenght <= length;
+function parseTime(timeString) {
+  const [hour, minute] = timeString.split(':');
+  return hour * 60 + Number(minute);
 }
-lineSize('проверяемая строка', 20);
-function isPalindrome (str){
-  const string = str.replaceAll(' ', '').toLowerCase();
-  for (let n = 0; n < string.length / 2; n++){
-    if (string.at(n) !== string.at(- n - 1)){
-      return false;
-    }
-  }
-  return true;
+function chekMeeting(dayStart, dayEnd, meetingStart, meetingDuration) {
+  const dayStartInMinutes = parseTime(dayStart);
+  const dayEndInMinutes = parseTime(meetingStart);
+  const meetingStartInMinutes = parseTime(meetingStart);
+  return (
+    meetingStartInMinutes >= dayStartInMinutes &&
+    meetingStartInMinutes + meetingDuration <= dayEndInMinutes
+  );
 }
-isPalindrome('топот');
-isPalindrome('ДовОд');
-isPalindrome('Кекс');
-isPalindrome('Лёша на полке клопа нашёл');
+chekMeeting('08:00', '17:30', '14:00', 90);
+chekMeeting('8:0', '10:0', '8:0', 120);
+chekMeeting('08:00', '14:30', '14:00', 90);
+chekMeeting('14:00', '17:30', '08:0', 90);
+chekMeeting('8:00', '17:30', '08:00', 900);
